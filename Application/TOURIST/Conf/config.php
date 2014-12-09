401f75448b5658c7c40a1666f0a4da865c9b8161
rororoororor <mingweiliusjtu@gmail.com>
2014-12-10 00:12:47 +0800

    add question part

--- Application/TOURIST/Conf/config.php
+++ Application/TOURIST/Conf/config.php
@@ -12,6 +12,8 @@ define('_TBL_VISIT_SITE_', 'visit_site');
 define('_TBL_SITE_TREE_', 'site_tree');
 define('_TBL_ITEM_KEY_VALUE_', 'item_key_value');
 define('_TBL_CITY_', 'province_city');
+define('_TBL_SITE_QUESTION_', 'site_question');
+define('_TBL_FEEDBACK_QUESTION_', 'feedback_question');
 
 return array(
 		
